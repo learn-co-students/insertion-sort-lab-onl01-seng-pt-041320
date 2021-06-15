aac48c290f8754e4d54cae2b29ce23ca2aaaf36f
chica25 <pfco520@gmail.com>
2021-06-14 21:05:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
-
+    let minNum = array[0];
+    let minIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < minNum) {
+        minNum = array[i];
+        minIndex = i;
+      }
+    }
+    array.splice(minIndex, 1);
+    return minNum;
 }
 
-function selectionSort(array){
 
-}
+function selectionSort(array) {
+    let sorted = [];
+    let newMin;
+    
+    while (array.length != 0) {
+      newMin = findMinAndRemove(array);
+      sorted.push(newMin);
+    }
+    return sorted;
+  }
